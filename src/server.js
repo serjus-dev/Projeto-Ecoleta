68e87c91e0beb318f225aba20375bbcc5b132ff5
serjus <serjus.dev@gmail.com>
2020-06-07 14:43:24 -0300

    Configuração servidor, instalação pacotes/dependencias, configuração de rotas

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+
+const server = express();
+
+//CONFIGURAR PASTA PUBLIC
+server.use(express.static("public"))
+
+
+// UTILIZANDO TEMPLATE ENGINE
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+//CONFIGURAR CAMINHOS DA MINHA APLICAÇÃO
+//PAGINA INICIAL 
+// são as rotas 
+server.get("/", (req,res) => {
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req,res) => {
+    return res.render("create-point.html")
+})
+
+
+// LIGAR O SERVIDOR
+server.listen(3000)
+
+
